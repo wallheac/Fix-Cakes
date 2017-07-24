31729b838aa7663618857076f8ce0e6bab619ca2
Amy Wallhermfechtel <wallheac@gmail.com>
2017-07-23 20:44:09 -0500

    fixed overlap between hero and content

--- script.js
+++ script.js
@@ -18,10 +18,35 @@ $(document).ready(function(){
     if($(this).scrollTop() > (hdr/3)){
       $('nav').addClass('scrolled');
       $('#logo').addClass('scrolled');
+      event.preventDefault();
     }
     else{
       $('nav').removeClass('scrolled');
       $('#logo').removeClass('scrolled');
     }
   });
+
+  //edit nav in jquery
+  $('nav').html("<ul><li><a href='menu-cakes.html'>menu</a></li><li><a href='custom.html'>custom</a></li></ul><a href='index.html'><img id='logo' src='./assets/images/fix-logo.png' alt='fix cakes logo'></a></li><ul><li><a href='about.html'>about</a></li><li><a href='hours.html'>hours</a></li></ul>");
+  $('footer').html("<div class='address'>    <p>27 Maryland Plaza<br>      Saint Louis, MO 63108</p>     <p>Tues-Sat 7am-7pm, Sun 11am-4pm</p>  </div>  <img id='footerLogo' src='./assets/images/fix-logo-white.png'><div class='fti'><div id='ftiIcons'><img src='./assets/images/fix-fbicon.png' alt='facebook icon'><img src='./assets/images/fix-twittericon.png' alt='twitter icon'><img src='./assets/images/fix-instaicon.png' alt='instagram icon'></div><p>314-930-3103</p></div>")
+
+//JSON inventory
+$.getJSON("https://wallheac.github.io/Fix-Cakes/inventory.json", function(data){
+    var cakeItems = [],
+      otherItems = [],
+      cakeString = '',
+      otherString = '';
+  $.each(data, function(index, val){//sort cakes from other
+    if(val.type == "Cake"){cakeItems.push(val);}
+    if(val.type == "Other") {otherItems.push(val);}
+  });
+  $.each(cakeItems, function(index,val){
+    cakeString += ("<h2>" + val.name + "</h2>" + "<p>" + val.description +"</p>");
+  });
+  $.each(otherItems, function(index,val){
+    otherString += ("<h2>" + val.name + "</h2>" + "<p>" + val.description +"</p>");
+  });
+  $('#cakeMenu p').first().append(cakeString);
+  $('#otherMenu p').first().append(otherString);
+});
 });
